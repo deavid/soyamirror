32016ce0e8ff56fd8b8823ac1484b216f48a7acd
jiba <jiba@f7f35755-3713-0410-b059-dae289c688fa>
2008-01-20 13:10:42 +0000

    0.14rc1
    git-svn-id: svn://svn.gna.org/svn/soya/trunk@436 f7f35755-3713-0410-b059-dae289c688fa

--- soya/setup.py
+++ soya/setup.py
@@ -255,7 +255,7 @@ else:
 
 setup(
 	name         = "Soya",
-	version      = "0.13.2",
+	version      = "0.14rc1",
 	license      = "GPL",
 	description  = "A practical high-level object-oriented 3D engine for Python.",
 	long_description  = """A practical high-level object-oriented 3D engine for Python.
